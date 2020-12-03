0015b9d1015a38078bdbb2ad25500532027aa5c9
liebealles <67030978+LIEBEALLES@users.noreply.github.com>
2020-12-04 04:48:00 +0900

    가운데 글자 가져오기

---
+++ Solution08.java
@@ -0,0 +1,30 @@
+package Test;
+
+public class Solution08 {
+	// 가운데 글자 가져오기
+	/*
+	 * 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 
+	 * 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+	 * 
+	 */
+	public static String solution(String s) {
+		//글자 수가 홀수일 때 짝수일 때 생각
+		String answer = "";
+		
+		if(s.length()%2 ==0) {
+			//짝수
+			answer = s.substring(s.length()/2-1, s.length()/2+1);
+		}else {
+			//홀수
+			answer = s.substring(s.length()/2, s.length()/2+1);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String s = "abcdefg";
+		//String s1 = "qwer";
+		System.out.println(solution(s));
+		//System.out.println(solution(s1));
+	}
+}
